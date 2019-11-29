f53e478d6536f264534fe6d887092199b9aebc5d
zhanxuzhao <649912323@qq.com>
2019-11-29 11:49:32 +0800

    update MainActivity;

--- app/src/main/java/com/zxz/androiduicomponents/MainActivity.kt
+++ app/src/main/java/com/zxz/androiduicomponents/MainActivity.kt
@@ -2,11 +2,21 @@ package com.zxz.androiduicomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zxz.ui_components.EasyDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv.setOnClickListener {
+            val dialog = EasyDialog(this)
+            dialog.setTitle(R.string.app_name)
+            dialog.show()
+
+
+        }
     }
 }
