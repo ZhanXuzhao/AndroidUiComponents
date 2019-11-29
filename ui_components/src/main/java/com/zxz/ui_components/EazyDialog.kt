a9e29ac842d2f383f3327502ef90ff07a3e7d0a4
zhanxuzhao <649912323@qq.com>
2019-11-29 11:13:55 +0800

    add ui_components;

---
+++ ui_components/src/main/java/com/zxz/ui_components/EazyDialog.kt
@@ -0,0 +1,15 @@
+package com.zxz.ui_components
+
+import android.app.Dialog
+import android.content.Context
+
+/**
+ * desc   :
+ * version: 1.0
+ * @author : Zhan Xuzhao
+ * e-mail : 649912323@qq.com
+ * time   : 2019-11-29 11:12
+ */
+class EasyDialog(context: Context) : Dialog(context) {
+
+}
